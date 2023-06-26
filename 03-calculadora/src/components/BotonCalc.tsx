6b6a54103c6fecddea10a2b4fcf0debc7d685ad0
Dante <dante.nkaddarian@gmail.com>
2023-06-26 18:08:13 -0300

    creado boton 0, agregada funcionalidad de limpiar pantalla y de formar numeros

--- 03-calculadora/src/components/BotonCalc.tsx
+++ 03-calculadora/src/components/BotonCalc.tsx
@@ -1,17 +1,27 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { Text, TouchableOpacity, View } from 'react-native'
 import { styles } from '../theme/appTheme'
 
 interface Props {
     texto: string,
-    color: string
+    color: string,
+    ancho?: boolean,
+    accion: (numeroTexto: string) => void // todos los botones van a tener una accion
 }
 
-export const BotonCalc = ({ texto, color }: Props) => {
+export const BotonCalc = ({ texto, color, ancho = false, accion }: Props) => {
     return (
-        <View style={{ ...styles.boton, backgroundColor: color }}>
-            <Text style={styles.botonTexto}>{texto}</Text>
-        </View>
+        <TouchableOpacity
+            onPress={() => accion(texto)}>
+            <View style={{
+                ...styles.boton,
+                backgroundColor: color,
+                width: ancho ? 180 : 80
+            }}>
+                <Text
+                    style={styles.botonTexto}>{texto}</Text>
+            </View>
+        </TouchableOpacity>
     )
 }
 
--- 03-calculadora/src/screen/CalculadoraScreen.tsx
+++ 03-calculadora/src/screen/CalculadoraScreen.tsx
@@ -1,49 +1,65 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Text, View } from 'react-native'
 import { styles } from '../theme/appTheme'
 import { BotonCalc } from '../components/BotonCalc'
 
 export const CalculadoraScreen = () => {
+
+    const [numero, setNumero] = useState('100')
+    const [numeroAnterior, setnumeroAnterior] = useState('0')
+
+    const limpiar = () => {
+        setNumero('0')
+    }
+
+    const armarNumero = (numeroTexto: string) => {
+        setNumero(numero + numeroTexto)
+    }
+
+
     return (
         <View style={styles.calculadoraContainer}>
             <Text style={styles.resultadoPequeno}>
-                1,500.00
+                {numeroAnterior}
             </Text>
-            <Text style={styles.resultado}>
-                1,500.00
+            <Text
+                style={styles.resultado}
+                numberOfLines={1}
+                adjustsFontSizeToFit={true}>
+                {numero}
             </Text>
             <View style={styles.fila}>
                 {/* Boton */}
-                <BotonCalc texto="C" color={styles.grisClaro.backgroundColor} />
-                <BotonCalc texto="+/-" color={styles.grisClaro.backgroundColor} />
-                <BotonCalc texto="del" color={styles.grisClaro.backgroundColor} />
-                <BotonCalc texto="/" color={styles.naranja.backgroundColor} />
+                <BotonCalc texto="C" color={styles.grisClaro.backgroundColor} accion={limpiar} />
+                <BotonCalc texto="+/-" color={styles.grisClaro.backgroundColor} accion={limpiar} />
+                <BotonCalc texto="del" color={styles.grisClaro.backgroundColor} accion={limpiar} />
+                <BotonCalc texto="/" color={styles.naranja.backgroundColor} accion={limpiar} />
             </View>
             {/* #2D2D2D gris oscuro */}
             <View style={styles.fila}>
-                <BotonCalc texto="7" color={styles.grisOscuro.backgroundColor} />
-                <BotonCalc texto="8" color={styles.grisOscuro.backgroundColor} />
-                <BotonCalc texto="9" color={styles.grisOscuro.backgroundColor} />
-                <BotonCalc texto="X" color={styles.naranja.backgroundColor} />
+                <BotonCalc texto="7" color={styles.grisOscuro.backgroundColor} accion={armarNumero} />
+                <BotonCalc texto="8" color={styles.grisOscuro.backgroundColor} accion={armarNumero} />
+                <BotonCalc texto="9" color={styles.grisOscuro.backgroundColor} accion={armarNumero} />
+                <BotonCalc texto="X" color={styles.naranja.backgroundColor} accion={limpiar} />
             </View>
             <View style={styles.fila}>
-                <BotonCalc texto="4" color={styles.grisOscuro.backgroundColor} />
-                <BotonCalc texto="5" color={styles.grisOscuro.backgroundColor} />
-                <BotonCalc texto="6" color={styles.grisOscuro.backgroundColor} />
-                <BotonCalc texto="-" color={styles.naranja.backgroundColor} />
+                <BotonCalc texto="4" color={styles.grisOscuro.backgroundColor} accion={armarNumero} />
+                <BotonCalc texto="5" color={styles.grisOscuro.backgroundColor} accion={armarNumero} />
+                <BotonCalc texto="6" color={styles.grisOscuro.backgroundColor} accion={armarNumero} />
+                <BotonCalc texto="-" color={styles.naranja.backgroundColor} accion={limpiar} />
             </View>
             <View style={styles.fila}>
-                <BotonCalc texto="1" color={styles.grisOscuro.backgroundColor} />
-                <BotonCalc texto="2" color={styles.grisOscuro.backgroundColor} />
-                <BotonCalc texto="3" color={styles.grisOscuro.backgroundColor} />
-                <BotonCalc texto="+" color={styles.naranja.backgroundColor} />
+                <BotonCalc texto="1" color={styles.grisOscuro.backgroundColor} accion={armarNumero} />
+                <BotonCalc texto="2" color={styles.grisOscuro.backgroundColor} accion={armarNumero} />
+                <BotonCalc texto="3" color={styles.grisOscuro.backgroundColor} accion={armarNumero} />
+                <BotonCalc texto="+" color={styles.naranja.backgroundColor} accion={limpiar} />
             </View>
             {/* #FF9427  naranja */}
             <View style={styles.fila}>
-
-                <BotonCalc texto="0" color={styles.grisOscuro.backgroundColor} />
-                <BotonCalc texto="." color={styles.grisOscuro.backgroundColor} />
-                <BotonCalc texto="=" color={styles.naranja.backgroundColor} />
+                {/* si a ancho no le envio nada, asume que es true */}
+                <BotonCalc texto="0" color={styles.grisOscuro.backgroundColor} ancho accion={armarNumero} />
+                <BotonCalc texto="." color={styles.grisOscuro.backgroundColor} accion={limpiar} />
+                <BotonCalc texto="=" color={styles.naranja.backgroundColor} accion={limpiar} />
             </View>
         </View >
     )
