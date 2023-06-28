04524c248fb90e693dbc1d44240946b168ada121
Dante <dante.nkaddarian@gmail.com>
2023-06-28 20:37:22 -0300

    iniciacion del proyecto 04-Navegacion-App

---
+++ 04-Navegacion-App/App.tsx
@@ -0,0 +1,27 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { Text } from 'react-native';
+
+function App(): JSX.Element {
+
+  return (
+    <NavigationContainer>
+      <Text>
+        Hola
+      </Text>
+    </NavigationContainer>
+  );
+}
+
+/* const styles = StyleSheet.create({
+}); */
+
+export default App;
