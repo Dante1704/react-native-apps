1dfdbf5e053d5c55c5275183000ca43a6ab46da6
Dante <dante.nkaddarian@gmail.com>
2023-07-06 17:58:41 -0300

    configuraciones basicas del menu de Tabs

--- 04-Navegacion-App/src/navigator/Tabs.tsx
+++ 04-Navegacion-App/src/navigator/Tabs.tsx
@@ -2,16 +2,33 @@ import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Tab1Screen } from '../screens/Tab1Screen';
 import { Tab2Screen } from '../screens/Tab2Screen';
-import { Tab3Screen } from '../screens/Tab3Screen';
+//import { Tab3Screen } from '../screens/Tab3Screen';
+import { StackNavigator } from './StackNavigator';
+import { colores } from '../theme/appTheme';
 
 const Tab = createBottomTabNavigator();
-
+//OJO! cuando yo me muevo entre tabs, los useEffect que los mismos tengan solo se van a disparar la primera vez que muevo, luego ya no
 export function Tabs() {
     return (
-        <Tab.Navigator>
-            <Tab.Screen name="Tab1Screen" component={Tab1Screen} />
-            <Tab.Screen name="Tab2Screen" component={Tab2Screen} />
-            <Tab.Screen name="Tab3Screen" component={Tab3Screen} />
+        <Tab.Navigator
+            //el style del screen excepto el menu de tabs
+            sceneContainerStyle={{
+                backgroundColor: 'white',
+            }}
+            screenOptions={{
+                tabBarActiveTintColor: colores.primary, //a la tab activa le pone color red
+                //para estilar las tabs
+                tabBarStyle: {
+                    borderTopColor: colores.primary, //borde superior del menu de tabs
+                    //con las siguientes dos propiedades evito que haya una separacion entre el menu de tabs y el resto del screen
+                    borderTopWidth: 0,
+                    elevation: 0,
+                },
+            }}>
+            {/* headerShown: false si no quiero mostrar el header que dice tab1 */}
+            <Tab.Screen /* options={{ headerShown: false }} */ name="Tab1" component={Tab1Screen} />
+            <Tab.Screen name="Tab2" component={Tab2Screen} />
+            <Tab.Screen name="StackNavigator" component={StackNavigator} />
         </Tab.Navigator>
     );
 }
--- 04-Navegacion-App/src/screens/Tab3Screen.tsx
+++ 04-Navegacion-App/src/screens/Tab3Screen.tsx
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Text, View } from 'react-native';
 
 export const Tab3Screen = () => {
+
+    useEffect(() => {
+        console.log('Tab3Screen Effect');
+    }, []);
+
     return (
         <View>
             <Text>Tab3Screen</Text>
