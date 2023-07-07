9849919cba2195f8117593462cd8ee58ef84938c
Dante <dante.nkaddarian@gmail.com>
2023-07-07 17:56:19 -0300

    instalado y creado Material bottom tab navigation para el caso de android

--- 04-Navegacion-App/src/navigator/Tabs.tsx
+++ 04-Navegacion-App/src/navigator/Tabs.tsx
@@ -1,17 +1,77 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 import { Tab1Screen } from '../screens/Tab1Screen';
 import { Tab2Screen } from '../screens/Tab2Screen';
 //import { Tab3Screen } from '../screens/Tab3Screen';
 import { StackNavigator } from './StackNavigator';
 import { colores } from '../theme/appTheme';
-import { Text } from 'react-native';
+import { Platform, Text } from 'react-native';
 
-const Tab = createBottomTabNavigator();
-//OJO! cuando yo me muevo entre tabs, los useEffect que los mismos tengan solo se van a disparar la primera vez que muevo, luego ya no
-export function Tabs() {
+
+export const Tabs = () => {
+
+    return Platform.OS === 'ios'
+        ? <TabIOS />
+        : <TabsAndroid />;
+};
+
+
+
+
+const BottomTabAndroid = createMaterialBottomTabNavigator();
+
+function TabsAndroid() {
     return (
-        <Tab.Navigator
+        <BottomTabAndroid.Navigator
+            sceneAnimationEnabled={false}
+            barStyle={{ backgroundColor: colores.primary }}
+            screenOptions={
+                //manera centralizada de controlar todos los iconos para no tener tanto codigo en cada Tab.Screen
+                ({ route }) => ({
+                    // eslint-disable-next-line react/no-unstable-nested-components
+                    tabBarIcon: ({ color/* , focused, size  */ }) => {
+                        let iconName: string = '';
+                        switch (route.name) {
+                            case 'Tab1':
+                                iconName = 'T1';
+                                break;
+                            case 'Tab2':
+                                iconName = 'T2';
+                                break;
+                            case 'StackNavigator':
+                                iconName = 'StNav';
+                                break;
+                        }
+                        return <Text style={{ color }}>{iconName}</Text>;
+                    },
+                    tabBarActiveTintColor: colores.primary, //a la tab activa le pone color morado
+                    //para estilar las tabs
+                    tabBarStyle: {
+                        borderTopColor: colores.primary, //borde superior del menu de tabs
+                        //con las siguientes dos propiedades evito que haya una separacion entre el menu de tabs y el resto del screen
+                        borderTopWidth: 0,
+                        elevation: 0,
+                    },
+                    tabBarLabelStyle: {
+                        fontSize: 15,
+                    },
+                })}>
+            <BottomTabAndroid.Screen name="Tab1" component={Tab1Screen} />
+            <BottomTabAndroid.Screen name="Tab2" component={Tab2Screen} />
+            <BottomTabAndroid.Screen name="StackNavigator" component={StackNavigator} />
+        </BottomTabAndroid.Navigator>
+    );
+}
+
+
+
+
+const BottomTabIOS = createBottomTabNavigator();
+//OJO! cuando yo me muevo entre tabs, los useEffect que los mismos tengan solo se van a disparar la primera vez que muevo, luego ya no
+function TabIOS() {
+    return (
+        <BottomTabIOS.Navigator
             //el style del screen excepto el menu de tabs
             sceneContainerStyle={{
                 backgroundColor: 'white',
@@ -49,17 +109,17 @@ export function Tabs() {
                 })}>
             {/* headerShown: false si no quiero mostrar el header que dice tab1 */}
             {/* <Tab.Screen
-                // manera individual en cada Tab.Screen de poner un icono
                 options={{
                     headerShown: false,
+                    // manera individual en cada Tab.Screen de poner un icono
                     tabBarIcon: (props) => <Text style={{ color: props.color }}>T1</Text>, //props.color el color que le hereda el padre
                 }}
                 name="Tab1"
                 component={Tab1Screen}
             /> */}
-            <Tab.Screen name="Tab1" component={Tab1Screen} />
-            <Tab.Screen name="Tab2" component={Tab2Screen} />
-            <Tab.Screen name="StackNavigator" component={StackNavigator} />
-        </Tab.Navigator>
+            <BottomTabIOS.Screen name="Tab1" component={Tab1Screen} />
+            <BottomTabIOS.Screen name="Tab2" component={Tab2Screen} />
+            <BottomTabIOS.Screen name="StackNavigator" component={StackNavigator} />
+        </BottomTabIOS.Navigator>
     );
 }
