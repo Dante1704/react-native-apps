186b128ee5bfceff3119d466148549cd20986937
Dante <dante.nkaddarian@gmail.com>
2023-07-13 17:45:40 -0300

    renderizado de los valores del context en el componente SettingsScreen

--- 04-Navegacion-App/src/context/AuthContext.tsx
+++ 04-Navegacion-App/src/context/AuthContext.tsx
@@ -1,25 +1,29 @@
 import React, { createContext } from 'react';
 
-
+//defino la informacion que va a tener el context object
 export interface AuthState {
     isLoggedIn: boolean;
     username?: string;
     favoriteIcon?: string;
 }
 
+// defino un estado global para cuando inicia la aplicacion
 export const authInitialState: AuthState = {
     isLoggedIn: false,
     username: undefined,
     favoriteIcon: undefined,
 };
 
+//defino las props de lo que va a exponer el context a los componentes suscriptos
 export interface AuthContextProps {
     authState: AuthState;
     signIn: () => void;
 }
 
+//creo el context
 export const AuthContext = createContext({} as AuthContextProps);
 
+//creo el proveedor del estado que es el encargado de suscribir los componentes que yo desee al estado global
 export const AuthProvider = ({ children }: any) => {
     return (
         <AuthContext.Provider value={{ authState: authInitialState, signIn: () => { } }}>
--- 04-Navegacion-App/src/screens/SettingsScreen.tsx
+++ 04-Navegacion-App/src/screens/SettingsScreen.tsx
@@ -1,19 +1,24 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Text, View } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { styles } from '../theme/appTheme';
+import { AuthContext } from '../context/AuthContext';
 
 export const SettingsScreen = () => {
 
     //useSafeAreaInsets me calcula los espacios en los que no se debe mostrar contenido en las 4 direcciones
     const insets = useSafeAreaInsets();
 
+    //de esta manera utilizo el context en este componente
+    const { authState } = useContext(AuthContext);
+
     return (
         //usa de margin todo el espacio que no se debe mostrar contenido, en este caso el margin superior
         //especialmente util para iOS por el notch que tiene en la parte de arriba los mobile apple
         //de esta manera no necesito usar el componente SafeAreaView
         <View style={{ ...styles.globalMargin, marginTop: insets.top + 20 }}>
-            <Text style={styles.title}>SettingsScreen</Text>
+            <Text style={styles.title}>Settings Screen</Text>
+            <Text>{JSON.stringify(authState, null, 4)}</Text>
         </View>
     );
 };
