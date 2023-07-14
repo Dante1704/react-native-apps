4a5f7553d8d12a82f57bcae7b18a1f96222c0c96
Dante <dante.nkaddarian@gmail.com>
2023-07-14 16:57:05 -0300

    creada funcionalidad de loguearse yrenderizado condicional del boton que ejecuta la accion de log in

--- 04-Navegacion-App/src/screens/ContactsScreen.tsx
+++ 04-Navegacion-App/src/screens/ContactsScreen.tsx
@@ -1,10 +1,22 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useContext } from 'react';
+import { Button, Text, View } from 'react-native';
+import { styles } from '../theme/appTheme';
+import { AuthContext } from '../context/AuthContext';
 
 export const ContactsScreen = () => {
+
+    //doble destructuring
+    const { authState: { isLoggedIn }, signIn } = useContext(AuthContext);
+
     return (
-        <View>
-            <Text>ContactsScreen</Text>
+        <View style={styles.globalMargin}>
+            <Text style={styles.title}>Contacts Screen</Text>
+            {/* si estoy logueado no muestro el boton */}
+            {!isLoggedIn &&
+                <Button
+                    title="Sign In"
+                    onPress={signIn}
+                />}
         </View>
     );
 };
