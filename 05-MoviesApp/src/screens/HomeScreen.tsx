38c571e6d98b4df75905ede43af47b023b95e30a
Dante <dante.nkaddarian@gmail.com>
2023-08-01 14:33:33 -0300

    extraidos los colores del poster de la pelicula. react-native-image-colors se instala por defecto con una version que no es compatible. Lo solucione instalando la version ^1.5.2 y es la ultima version comprobada que funciona a menos que se instale expo y el proyecto se corra ahi.

--- 05-MoviesApp/src/screens/HomeScreen.tsx
+++ 05-MoviesApp/src/screens/HomeScreen.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { View, useWindowDimensions, ScrollView } from 'react-native';
 //import { useNavigation } from '@react-navigation/core';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import ImageColors from 'react-native-image-colors';
 import { useMovies } from '../hooks/useMovies';
 import { Loading } from '../components/Loading';
 import { MoviePoster } from '../components/MoviePoster';
-import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import Carousel from 'react-native-snap-carousel';
 import { HorizontalSlider } from '../components/HorizontalSlider';
 import { GradientBackground } from '../components/GradientBackground';
@@ -17,6 +18,17 @@ export const HomeScreen = () => {
     const { width } = useWindowDimensions();
     const { top } = useSafeAreaInsets(); //para no renderizar sobre el notch de iOS
     const { nowPlaying, popular, topRated, upcoming, isLoading } = useMovies();
+
+    const getPosterColors = async (index: number) => {
+        const currentMovie = nowPlaying[index];
+        const uri = `https://image.tmdb.org/t/p/w500${currentMovie.poster_path}`;
+        console.log(uri);
+        const colors = await ImageColors.getColors(uri, {
+            fallback: '#FFF',
+        });
+        console.log('colors:', colors);
+
+    };
 
     if (isLoading) {
         return (
@@ -46,6 +58,10 @@ export const HomeScreen = () => {
                         itemWidth={300}
                         inactiveSlideOpacity={0.9}
                         vertical={false} //sin esto no funciona
+
+                        //esta funcion se ejecuta cuando deslizo y aparece el siguiente elemento
+                        //en este caso es el siguiente poster
+                        onSnapToItem={index => getPosterColors(index)}
                     />
                 </ View>
                 {/* peliculas populares*/}
