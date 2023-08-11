57ef7b28ae5aaf483acf6907c85e9a29ac37c65c
Dante <dante.nkaddarian@gmail.com>
2023-08-11 14:58:30 -0300

    creadas animaciones de fadeIn y fadeOut para el cubo del screen Animation101

--- 06-RNComponents/src/screens/Animation101Screen.tsx
+++ 06-RNComponents/src/screens/Animation101Screen.tsx
@@ -1,13 +1,39 @@
 import React, { useRef } from 'react';
-import { Animated, StyleSheet, View } from 'react-native';
+import { Animated, Button, StyleSheet, View } from 'react-native';
 
 export const Animation101Screen = () => {
 
-    const opacity = useRef(new Animated.Value(0.4)).current;
+    const opacity = useRef(new Animated.Value(0)).current;
+
+    const fadeIn = () => {
+        Animated.timing(
+            opacity, {
+            toValue: 1,
+            duration: 300,
+            useNativeDriver: true,
+        }).start();
+    };
+
+    const fadeOut = () => {
+        Animated.timing(
+            opacity, {
+            toValue: 0,
+            duration: 300,
+            useNativeDriver: true,
+        }).start();
+    };
 
     return (
         <View style={styles.container}>
             <Animated.View style={{ ...styles.purpleBox, opacity: opacity }} />
+            <Button
+                title="fadeIn"
+                onPress={fadeIn}
+            />
+            <Button
+                title="fadeOut"
+                onPress={fadeOut}
+            />
         </View>
     );
 };
