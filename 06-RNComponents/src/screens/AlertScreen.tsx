ab6f203a4fdc19e7ece15e748a22b34fccc536f1
Dante <dante.nkaddarian@gmail.com>
2023-08-15 16:01:02 -0300

    Instalado paquete react-native-prompt-android. Permite hacer alertas de prompts en android ya que Alert.promp() solo esta disponible en iOS

--- 06-RNComponents/src/screens/AlertScreen.tsx
+++ 06-RNComponents/src/screens/AlertScreen.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import prompt from 'react-native-prompt-android';
 import { HeaderTitle } from '../components/HeaderTitle';
 import { Alert, Button, View } from 'react-native';
 import { styles } from '../theme/Theme';
@@ -19,14 +20,36 @@ export const AlertScreen = () => {
             //opcional, para cerrar la alerta cuando toco en otra parte de la pantalla
             //la idea es no ponerlo porque queremos obligar al usuario que elija una opcion
             cancelable: true,
-        });
+        }
+    );
+
+    const showPromptAndroid = () => prompt(
+        'Title Here',
+        'Prompt Message Here',
+        [
+            { text: 'Cancel', onPress: () => console.log('Cancel Pressed'), style: 'cancel' },
+            { text: 'OK', onPress: password => console.log('OK Pressed, password: ' + password) },
+        ],
+        {
+            type: 'secure-text',
+            cancelable: false,
+            defaultValue: 'test',
+            placeholder: 'placeholder',
+        }
+    );
 
     return (
         <View style={styles.globalMargin}>
             <HeaderTitle title="AlertScreen" />
             <Button
                 title="Mostrar Alerta"
-                onPress={showAlert} />
+                onPress={showAlert}
+            />
+            <View style={{ height: 20 }} />
+            <Button
+                title="Mostrar Alerta"
+                onPress={showPromptAndroid}
+            />
         </View>
     );
 };
