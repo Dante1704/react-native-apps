4834d0c7feddbbee98ead400fd5dc989dbf7ee50
Dante <dante.nkaddarian@gmail.com>
2023-08-15 18:04:03 -0300

    src/screens/TextInput - Combinando los componentes KeyboardingView, ScrollView, y TouchableWithoutFeedback se maneja el comportamiento del teclado que aparece para escribir.

--- 06-RNComponents/src/screens/TextInputScreen.tsx
+++ 06-RNComponents/src/screens/TextInputScreen.tsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import { HeaderTitle } from '../components/HeaderTitle';
 import { styles } from '../theme/Theme';
-import { View, TextInput, StyleSheet } from 'react-native';
+import { View, TextInput, StyleSheet, KeyboardAvoidingView, Platform, TouchableWithoutFeedback, Keyboard, Button } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
 
 export const TextInputScreen = () => {
 
@@ -18,31 +19,47 @@ export const TextInputScreen = () => {
         });
     };
     return (
-        <View style={styles.globalMargin}>
-            <HeaderTitle title="TextInputScreen" />
-            <TextInput
-                style={stylesScreen.inputStyle}
-                placeholder="Dante Kaddarian"
-                autoCorrect={false} //evitamos que autocorrija lo que escribimos.
-                autoCapitalize="words"
-                onChangeText={(value) => handleChange(value, 'name')} //aca no esta por defecto el evento onChange
-            />
-            <TextInput
-                style={stylesScreen.inputStyle}
-                placeholder="dante@dante.com"
-                autoCorrect={false}
-                onChangeText={(value) => handleChange(value, 'email')}
-                keyboardType="email-address" //email-address me permite tener el @ sin tener que cambiar a simbolos en el teclado
-            />
-            <TextInput
-                style={stylesScreen.inputStyle}
-                placeholder="+541134961991"
-                onChangeText={(value) => handleChange(value, 'phone')}
-                keyboardType="phone-pad" // phone-pad me muestra directamente numeros
-            />
+        <KeyboardAvoidingView
+            behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+        >
+            <ScrollView>
+                <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+                    <>
+                        <View style={{ ...styles.globalMargin, flex: 1 }}>
+                            <HeaderTitle title="TextInputScreen" />
+                            <TextInput
+                                style={stylesScreen.inputStyle}
+                                placeholder="Dante Kaddarian"
+                                autoCorrect={false} //evitamos que autocorrija lo que escribimos.
+                                autoCapitalize="words"
+                                onChangeText={(value) => handleChange(value, 'name')} //aca no esta por defecto el evento onChange
+                            />
+                            <TextInput
+                                style={stylesScreen.inputStyle}
+                                placeholder="dante@dante.com"
+                                autoCorrect={false}
+                                onChangeText={(value) => handleChange(value, 'email')}
+                                keyboardType="email-address" //email-address me permite tener el @ sin tener que cambiar a simbolos en el teclado
+                            />
 
-            <HeaderTitle title={JSON.stringify(form, null, 2)} />
-        </View>
+                            <HeaderTitle title={JSON.stringify(form, null, 2)} />
+                            <HeaderTitle title={JSON.stringify(form, null, 2)} />
+
+                            <TextInput
+                                style={stylesScreen.inputStyle}
+                                placeholder="+541134961991"
+                                onChangeText={(value) => handleChange(value, 'phone')}
+                                keyboardType="phone-pad" // phone-pad me muestra directamente numeros
+                            />
+                        </View>
+                        <View style={stylesScreen.btnContainer}>
+                            <Button title="Submit" onPress={() => null} />
+                        </View>
+                        <View style={{ height: 100 }} />
+                    </>
+                </TouchableWithoutFeedback>
+            </ScrollView>
+        </KeyboardAvoidingView>
     );
 };
 
@@ -54,5 +71,9 @@ const stylesScreen = StyleSheet.create({
         borderRadius: 10,
         borderColor: 'black',
         marginVertical: 10,
+    },
+    btnContainer: {
+        backgroundColor: 'white',
+        marginTop: 12,
     },
 });
