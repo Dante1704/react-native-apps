631b94eed13c90b78a2a3870408d7918368dd9a4
Dante <dante.nkaddarian@gmail.com>
2023-09-04 14:53:16 -0300

    src/context/ThemeContext - feature: implementado en el context la caracteristica que de la app inicie con el esquema de color que el usuario tiene seteado en un telefono. Además, si el usuario cambia el schema desde su telefono, tambien se va a actualizar en la app gracias al useEffect creado.

--- 06-RNComponents/src/context/themeContext/ThemeContext.tsx
+++ 06-RNComponents/src/context/themeContext/ThemeContext.tsx
@@ -1,5 +1,6 @@
-import React, { createContext, useReducer } from 'react';
-import { ThemeState, lightTheme, themeReducer } from './ThemeReducer';
+import React, { createContext, useEffect, useReducer } from 'react';
+import { ThemeState, darkTheme, lightTheme, themeReducer } from './ThemeReducer';
+import { useColorScheme } from 'react-native';
 
 interface ThemeContextProps {
     theme: ThemeState,
@@ -11,9 +12,17 @@ export const ThemeContext = createContext({} as ThemeContextProps);
 
 export const ThemeProvider = ({ children }: any) => {
 
+    const colorScheme = useColorScheme();  // esto me traer el theme que el usuario tiene en su OS.
 
+    const [theme, dispatch] = useReducer(themeReducer, (colorScheme === 'dark') ? darkTheme : lightTheme); // con esa condicion, la app va a iniciar con el esquema de color que tiene el usuario en su OS.
 
-    const [theme, dispatch] = useReducer(themeReducer, lightTheme);
+    //Con este useEffect voy a cambiar inmediatamente el esquema de color cada vez que el usuario lo cambie en la config de su telefono.
+    useEffect(() => {
+        (colorScheme === 'light')
+            ? setLightTheme()
+            : setDarkTheme();
+
+    }, [colorScheme]);
 
     const setDarkTheme = () => {
         dispatch({ type: 'set_dark_theme' });
