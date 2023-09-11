eb1e323bfe5bc7e50852c198600697d3d827d4fb
Dante <dante.nkaddarian@gmail.com>
2023-09-11 14:28:12 -0300

    hooks/usePokemonPaginated - feature: creada funcion mapPokemonListToSimplePokemon y creado el tipado para la respuesta que viene de la API de pokemon

--- 07-Pokedex/src/hooks/usePokemonPaginated.tsx
+++ 07-Pokedex/src/hooks/usePokemonPaginated.tsx
@@ -1,23 +1,39 @@
-import { useEffect, useRef } from 'react';
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useEffect, useRef, useState } from 'react';
+import { PokemonPaginatedResponse, Result, SimplePokemon } from '../interfaces/pokemonInterfaces';
 
 
 export const usePokemonPaginated = () => {
 
-
-    const urlNextPage = 'https://pokeapi.co/api/v2/pokemon/?offset=&limit=40';
+    const [simplePokemonList, setSimplePokemonList] = useState<SimplePokemon[]>([]);
     const nextPageUrl = useRef('https://pokeapi.co/api/v2/pokemon/?limit=40');
 
     const loadPokemons = async () => {
-        try {
-            const data = await fetch(nextPageUrl.current).then(response => response.json());
-            console.log(data);
-            nextPageUrl.current = data.next;
 
-        } catch (error) {
-            console.error(error);
-        }
+        fetch(nextPageUrl.current)
+            .then((response) => {
+                // Verificar si la respuesta HTTP tiene éxito (código 200)
+                if (!response.ok) {
+                    throw new Error(`Error en la solicitud: ${response.status} ${response.statusText}`);
+                }
+                // Parsear la respuesta JSON y tiparla
+                return response.json() as Promise<PokemonPaginatedResponse>; // Usamos 'as' para especificar el tipo
+            })
+            .then(data => {
+                nextPageUrl.current = data.next;
+                mapPokemonListToSimplePokemon(data.results);
+            })
+            .catch((error) => {
+                console.error('Error en la solicitud:', error);
+            });
 
     };
+
+    //esta funcion mapea la respuesta de un tipo hacia otro
+    const mapPokemonListToSimplePokemon = (pokemonList: Result[]) => {
+        pokemonList.map(poke => console.log(poke.name));
+    };
+
     useEffect(() => {
         loadPokemons();
     }, []);
