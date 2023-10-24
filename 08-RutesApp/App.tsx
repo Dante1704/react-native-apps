99681723a2e8b638e0d9b0973468c0cf80819703
Dante1704 <dante.nkaddarian@gmail.com>
2023-10-24 16:01:29 -0300

    creado esqueleto basico para la app de rutas

---
+++ 08-RutesApp/App.tsx
@@ -0,0 +1,21 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import Icon from 'react-native-vector-icons/Ionicons';
+
+import { Text, View } from 'react-native';
+
+const App = () => {
+  return (
+
+    <Icon name="rocket" size={30} color="#900" />
+
+  );
+};
+
+export default App;
