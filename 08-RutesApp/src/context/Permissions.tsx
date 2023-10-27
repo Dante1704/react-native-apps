721f149984a77ff42c330af3958e2f8dc1244d10
Dante1704 <dante.nkaddarian@gmail.com>
2023-10-27 17:25:11 -0300

    context/Permissions.tsx - feat: la app ya registra cuando hago un cambio en los permisos por fuera de ella

--- 08-RutesApp/src/context/Permissions.tsx
+++ 08-RutesApp/src/context/Permissions.tsx
@@ -1,7 +1,8 @@
-import React, { useState } from 'react';
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect, useState } from 'react';
 import { createContext } from 'react';
-import { Platform } from 'react-native';
-import { PERMISSIONS, PermissionStatus, request } from 'react-native-permissions';
+import { AppState, Platform } from 'react-native';
+import { PERMISSIONS, PermissionStatus, check, request } from 'react-native-permissions';
 
 export interface PermissionsState {
     locationStatus: PermissionStatus
@@ -23,9 +24,19 @@ export const PermissionsProvider = ({ children }: any) => {
 
     const [permissions, setPermissions] = useState(permissionsInitState);
 
+    //este useEffect se va a disparar solamente cuando se construye el provider. La app se costruye y destruye rapidamente cuando salgo de ella y vuelvo a entrar
+    //si yo por ejemplo salgo de mi app y por afuera de ella le cambio el permiso,
+    //al volver a entrar ya registra ese cambio que hice por fuera
+
+    useEffect(() => {
+        AppState.addEventListener('change', state => {
+            if (state !== 'active') { return; }
+            checkLocationPermission();
+        });
+    }, []);
+
+
     const askLocationPermission = async () => {
-
-
         let permissionsStatus: PermissionStatus;
 
         if (Platform.OS === 'ios') {
@@ -36,10 +47,25 @@ export const PermissionsProvider = ({ children }: any) => {
 
         setPermissions({ ...permissions, locationStatus: permissionsStatus });
         return permissionsStatus;
-
     };
 
-    const checkLocationPermission = () => { };
+    //cada vez que la persona se va de la aplicacion y despues vuelve, tengo que usar esta funcion.
+    //Porque pudo haber revocado los permisos
+    const checkLocationPermission = async () => {
+        let permissionsStatus: PermissionStatus;
+
+        if (Platform.OS === 'ios') {
+            permissionsStatus = await check(PERMISSIONS.IOS.LOCATION_WHEN_IN_USE); ////solicito permiso de location iOS
+            return permissionsStatus;
+        }
+        permissionsStatus = await check(PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION); //solicito permiso de location android
+
+        setPermissions({ ...permissions, locationStatus: permissionsStatus });
+        return permissionsStatus;
+    };
+
+
+
 
     return (
         <PermissionsContext.Provider
