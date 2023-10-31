a24b60521179e7f66b27f09eaca74f3a8c282ebd
Dante1704 <dante.nkaddarian@gmail.com>
2023-10-31 15:36:37 -0300

    configurado AndroidManifest.xml con la API key de Google Maps y generado el primer mapa con la API

--- 08-RutesApp/src/screens/Home.tsx
+++ 08-RutesApp/src/screens/Home.tsx
@@ -1,8 +1,30 @@
 import React from 'react';
-import Icon from 'react-native-vector-icons/Ionicons';
+import { StyleSheet, View } from 'react-native';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps'; // remove PROVIDER_GOOGLE import if not using Google Maps
 
-export const Home = () => {
-    return (
-        <Icon name="rocket" size={30} color="#900" />
-    );
-};
+const styles = StyleSheet.create({
+    container: {
+        ...StyleSheet.absoluteFillObject,
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    map: {
+        ...StyleSheet.absoluteFillObject,
+    },
+});
+
+export const Home = () => (
+    <View style={styles.container}>
+        <MapView
+            provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+            style={styles.map}
+            region={{
+                latitude: 37.78825,
+                longitude: -122.4324,
+                latitudeDelta: 0.015,
+                longitudeDelta: 0.0121,
+            }}
+        />
+    </View >
+);
